7df258662c0a2615c97a6ffa1e5e69740cc98e50
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-28 19:45:59 +0200

    changed script structure and addes config files

---
+++ main.js
@@ -0,0 +1,17 @@
+// importing settings from config.json
+import config from './config.json' assert { type: 'json' };
+console.log(data);
+
+// FUNCTION > change visibility of target-ID, visible/hidden, if hidden > direction to move
+/*function changeVis(target, visibility, direction) {
+    var queryTarget = document.querySelector('#' + target);
+    if (visibility == 'visible') {
+        queryTarget.classList.remove('hidden');
+        if (queryTarget.classList.contains('direction-up');)
+        queryTarget.classList.add('visible');
+    }
+}*/
+
+
+
+// element.classList.contains(class);
