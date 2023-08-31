10237ab8604e1402ffb8cb664f28625248c5ed4f
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 11:58:46 +0200

    changed video + text checker

--- bin/functions/checks.js
+++ bin/functions/checks.js
@@ -13,24 +13,34 @@ function checkVideo( modifier ){
     var httpReq = new XMLHttpRequest();
     httpReq.open('HEAD', config.video.blob + modifier + id + '.' + config.video.filetype + config.video.token);
     httpReq.send();
-    return( httpReq.status );
+    console.log( 'VIDEO STATUS => ' httpReq.status );
+    if (httpReq.status === 200) {
+        return( true );
+    } else {
+        return( false );
+    };
 }
 
 function checkText(){
     var httpReq = new XMLHttpRequest();
     httpReq.open('HEAD', config.video.blob + id + '.txt' + config.video.token);
     httpReq.send();
-    return( httpReq.status );
+    console.log( 'VIDEO STATUS => ' httpReq.status );
+    if (httpReq.status === 200) {
+        return( true );
+    } else {
+        return( false );
+    };
 }
 
 export function checkAll(){
     var statusVideo1zu1 = checkVideo( '1zu1' );
     var statusText = checkText();
-    console.log( 'VIDEO STATUS => ', statusVideo1zu1, statusVideo1zu1 );
-    if ( statusVideo1zu1 === 200 ){
+    //console.log( 'VIDEO STATUS => ', statusVideo1zu1, statusVideo1zu1 );
+    if ( statusVideo1zu1 == true ){
         goToSection( 'video' );
         loadVideo( id );
-    } else if ( statusText === 200 ){
+    } else if ( statusText == true ){
         goToSection( 'text' );
         setTimeout(() => { location.reload(); }, 10000);
     } else {
