589182f1cab447c671ffd962543cb4fd3186470f
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 11:19:03 +0200

    deleted error-video in config

--- bin/config.js
+++ bin/config.js
@@ -10,5 +10,5 @@ export const json = {
         "token": "?sp=r&st=2023-08-28T11:54:11Z&se=2023-09-29T19:54:11Z&spr=https&sv=2022-11-02&sr=c&sig=FTXGDQ08lHFWLn1gFG1D2wJILqHmQ3V7YnCcf32ABac%3D",
         "filetype": "mp4"
     },
-    "sections": [ "language", "mode", "record", "recording", "video", "text", "error-video", "error-id", "error-connection" ]
+    "sections": [ "language", "mode", "record", "recording", "video", "text", "error-connection", "error-id" ]
 };
