c1cf2138c8e6307994917b946ee19bfcc5bbd721
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 13:00:11 +0200

    integrated check iin video function

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -1,12 +1,12 @@
 // VIDEO LOADER
 
 import { json as config } from '../config.js';
-//import { checkVideo } from './checks.js';
+import { checkVideo } from './checks.js';
 
 export function loadVideo( id ){
     var video1zu1 = document.getElementById( 'player-video-1zu1' ).innerHTML;
     var button1zu1 = document.getElementById( 'button-video-1zu1' ).innerHTML;
-    video1zu1 = video1zu1.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + config.video.token );
+    video1zu1 = video1zu1.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + config.video.token, );
     button1zu1 = button1zu1.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + config.video.token );
     setTimeout(() => {
         var statusVideo = checkVideo( '' );
