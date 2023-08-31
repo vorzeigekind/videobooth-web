a86a06165969f651376cebe7c07f56cf412a8e79
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 13:15:49 +0200

    fixed video check

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -9,13 +9,11 @@ export function loadVideo( id ){
     var button1zu1 = document.getElementById( 'button-video-1zu1' ).innerHTML;
     video1zu1 = video1zu1.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + config.video.token, );
     button1zu1 = button1zu1.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + config.video.token );
-    setTimeout(() => {
-        var statusVideo = checkVideo();
-        console.log( 'CHECK' );
-        if ( statusVideo == true ){
-            var video = document.getElementById( 'button-video' );
-            video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + config.video.token );
-            video.classList.add( 'video-accessable' );
-        }
-    }, 60000);
+    var statusVideo = checkVideo();
+    console.log( 'CHECK' );
+    if ( statusVideo == true ){
+        var video = document.getElementById( 'button-video' );
+        video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + config.video.token );
+        video.classList.add( 'video-accessable' );
+    }
 };
