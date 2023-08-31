ba40e24bc9bac4360e551340a51f0754e77c9b78
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 12:53:37 +0200

    added scenes and texts

--- bin/customer.js
+++ bin/customer.js
@@ -9,26 +9,70 @@ export const json = {
             "text-language": "Get ready for some action and choose a language.",
             "button-language-0": "English",
             "button-language-1": "German",
+            "button-forward-language": "Next",
+            "button-back-mode": "Back",
             "title-mode": "Mode",
             "text-mode": "Mous bli bla blub",
             "title-singleplayer": "Singleplayer",
             "title-partymode": "Partymode",
-            "scene-0": "Szene 1",
-            "scene-1": "Szene 2",
-            "scene-2": "Szene 3"
+            "scene-0": "Scene 1",
+            "scene-1": "Scene 2",
+            "scene-2": "Scene 3",
+            "scene-3": "Scene 4",
+            "scene-4": "Scene 5",
+            "scene-5": "Scene 6",
+            "button-back-record": "Back",
+            "title-record": "Ready",
+            "text-record": "Text for record screen. Bla bla bla bla.",
+            "button-record": "Record",
+            "title-recording": "Recording",
+            "text-recording": "Text for recording screen. Bla bla bla bla. To check if it's ready click the button.",
+            "button-check": "Check Video",
+            "title-video": "Your Video",
+            "text-video": "Watch or download your video:",
+            "button-video-1zu1": "1:1 format",
+            "button-video": "16:9 format",
+            "title-text": "So close",
+            "text-text": "Your video is nearly finished. This page will reload in a few second.",
+            "title-error-id": "Invalid ID",
+            "text-error-id": "This ID is invalid. Simply rescan the QR-Code in the box!",
+            "title-error-connection": "Connection Error",
+            "text-error-connection": "Can't reach server."
         },
         "german": {
             "title-language": "Hallo, willkommen!",
-            "text-language": "Mach dich bereit für Action und wähl eine Sprache.",
-            "button-language-0": "Englisch",
-            "button-language-1": "Deutsch",
+            "text-language": "Get ready for some action and choose a language.",
+            "button-language-0": "English",
+            "button-language-1": "German",
+            "button-forward-language": "Next",
+            "button-back-mode": "Back",
             "title-mode": "Modus",
             "text-mode": "Mous bli bla blub",
             "title-singleplayer": "Singleplayer",
             "title-partymode": "Partymodus",
             "scene-0": "Szene 1",
             "scene-1": "Szene 2",
-            "scene-2": "Szene 3"
+            "scene-2": "Szene 3",
+            "scene-3": "Szene 4",
+            "scene-4": "Szene 5",
+            "scene-5": "Szene 6",
+            "button-back-record": "Back",
+            "title-record": "Los geht's",
+            "text-record": "Text for record screen. Bla bla bla bla.",
+            "button-record": "Aufnehmen",
+            "title-recording": "Aufnahme",
+            "text-recording": "Text for recording screen. Bla bla bla bla. To check if it's ready click the button.",
+            "button-check": "Check Video",
+            "title-video": "Dein Video",
+            "text-video": "Watch or download your video:",
+            "button-video-1zu1": "1:1 format",
+            "button-video": "16:9 format",
+            "title-text": "Fast fertig",
+            "text-text": "Your video is nearly finished. This page will reload in a few second.",
+            "title-error-id": "Ungültige ID",
+            "text-error-id": "This ID is invalid. Simply rescan the QR-Code in the box!",
+            "title-error-connection": "Verbindungs Fehler",
+            "text-error-connection": "Can't reach server."
         }
     },
     "scenes": {
@@ -43,6 +87,21 @@ export const json = {
         "2": {
             "mode": "singleplayer",
             "thumbnail": "Storymode.jpg"
+        }
+        ,
+        "3": {
+            "mode": "partymode",
+            "thumbnail": "Peak.jpg"
+        }
+        ,
+        "4": {
+            "mode": "partymode",
+            "thumbnail": "Talent.jpg"
+        }
+        ,
+        "5": {
+            "mode": "partymode",
+            "thumbnail": "Meet.jpg"
         }
     },
     "authors": ["Vorzeigekind GmbH", "Niklas Meyer", "Alexander Hesse"],
