8e00a1c09df53305da6dd29bc046fda004b16342
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 16:21:22 +0200

    added /

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -7,7 +7,7 @@ export function loadVideo( id ){
     console.log( 'LOADING VIDEO' );
     var video1zu1 = document.getElementById( 'player-video-1zu1' );
     var button1zu1 = document.getElementById( 'wrap-button-video-1zu1' );
-    video1zu1.innerHTML = video1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
+    video1zu1.innerHTML = video1zu1.innerHTML.replace( 'http://videolink1zu1/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     button1zu1.innerHTML = button1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     var statusVideo = checkVideo();
     console.log( 'CHECK' );
