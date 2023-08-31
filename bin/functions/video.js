e002b16891e30574cba79f906f3d22ca11294b4a
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 13:12:06 +0200

    added check log

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -11,6 +11,7 @@ export function loadVideo( id ){
     button1zu1 = button1zu1.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + config.video.token );
     setTimeout(() => {
         var statusVideo = checkVideo( '' );
+        console.log( 'CHECK' );
         if ( statusVideo == true ){
             var video = document.getElementById( 'button-video' );
             video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + config.video.token );
