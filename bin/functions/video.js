d5fd88d59f7fe74bc62dbc5cfb5560e6e15d878e
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 19:47:40 +0200

    added player src switch

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -5,11 +5,12 @@ import { checkVideo } from './checks.js';
 
 function loadVideo( id ){
     console.log( 'LOADING VIDEO' );
-    var video1zu1 = document.getElementById( 'player-video-1zu1' );
+    document.querySelector( '.elementor-video' ).src = config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token;
+    //var video1zu1 = document.getElementById( 'player-video-1zu1' );
     var button1zu1 = document.getElementById( 'wrap-button-video-1zu1' );
-    video1zu1.classList.add( 'video-accessable' );
+    //video1zu1.classList.add( 'video-accessable' );
     button1zu1.classList.add( 'video-accessable' );
-    video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
+    //video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     button1zu1.innerHTML = button1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     var statusVideo = checkVideo();
     console.log( 'CHECK' );
@@ -22,10 +23,10 @@ function loadVideo( id ){
 
 function switchLink( id ){
     console.log( 'CHANGING LINKS' );
-    var video1zu1 = document.getElementById( 'player-video-1zu1' );
+    //var video1zu1 = document.getElementById( 'player-video-1zu1' );
     var button1zu1 = document.getElementById( 'wrap-button-video-1zu1' );
     var video = document.getElementById( 'wrap-button-video' );
-    video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
+    //video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     button1zu1.innerHTML = button1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + '?' + config.video.token );
 }
