acee651eda29d5f411f9f65c288af1eb745c805b
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 12:05:51 +0200

    updated blob token

--- bin/config.js
+++ bin/config.js
@@ -7,7 +7,7 @@ export const json = {
     },
     "video": {
         "blob": "https://videoboothfiles.blob.core.windows.net/videos/",
-        "token": "?sp=r&st=2023-08-28T11:54:11Z&se=2023-09-29T19:54:11Z&spr=https&sv=2022-11-02&sr=c&sig=FTXGDQ08lHFWLn1gFG1D2wJILqHmQ3V7YnCcf32ABac%3D",
+        "token": "sp=r&st=2023-08-31T10:04:48Z&se=2024-01-17T19:04:48Z&sv=2022-11-02&sr=c&sig=wjQa2PHml%2F1nlezKPO3En2YmR7BkuPTHSEKd%2B4W0c9I%3D",
         "filetype": "mp4"
     },
     "sections": [ "loading", "language", "mode", "record", "recording", "video", "text", "error-connection", "error-id" ]
