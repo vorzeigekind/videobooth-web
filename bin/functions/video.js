325958aba2e5d069bfe7c8649ad21e8837042cc1
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 16:58:36 +0200

    changed src to switch

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -25,7 +25,7 @@ function switchLink( id ){
     var video1zu1 = document.getElementById( 'player-video-1zu1' );
     var button1zu1 = document.getElementById( 'wrap-button-video-1zu1' );
     var video = document.getElementById( 'wrap-button-video' );
-    video1zu1.innerHTML = video1zu1.innerHTML.replace( 'http://videolink1zu1/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
+    video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles.blob.core.windows.net/videos/1zu18642152121593.mp4?sp=r&st=2023-08-31T10:04:48Z&se=2024-01-17T19:04:48Z&sv=2022-11-02&sr=c&sig=wjQa2PHml%2F1nlezKPO3En2YmR7BkuPTHSEKd%2B4W0c9I%3D', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     button1zu1.innerHTML = button1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + '?' + config.video.token );
 }
