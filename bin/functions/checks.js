1dce776dcf322bfe5abc75ad7cf738df3ff25a2b
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 12:20:47 +0200

    fixed checks

--- bin/functions/checks.js
+++ bin/functions/checks.js
@@ -38,15 +38,13 @@ function checkText(){
 export function checkAll(){
     var httpTxt = new XMLHttpRequest();
     var httpVid = new XMLHttpRequest();
-    console.log( 'CHECKING STATUS => ' + config.video.blob + modifier + id + '.' + config.video.filetype + '?' + config.video.token );
-    console.log( 'CHECKING STATUS => ' + config.video.blob + modifier + id + '.' + config.video.filetype + '?' + config.video.token );
     httpTxt.open('HEAD', 'https://videoboothfiles.blob.core.windows.net/videos/' + id + '.txt?sp=r&st=2023-08-28T11:54:11Z&se=2023-09-29T19:54:11Z&spr=https&sv=2022-11-02&sr=c&sig=FTXGDQ08lHFWLn1gFG1D2wJILqHmQ3V7YnCcf32ABac%3D', false);
     httpVid.open('HEAD', 'https://videoboothfiles.blob.core.windows.net/videos/1zu1' + id + '.mp4?sp=r&st=2023-08-28T11:54:11Z&se=2023-09-29T19:54:11Z&spr=https&sv=2022-11-02&sr=c&sig=FTXGDQ08lHFWLn1gFG1D2wJILqHmQ3V7YnCcf32ABac%3D', false);
     httpTxt.send();
     httpVid.send();
     console.log( 'VIDEO STATUS => ' + httpTxt.status );
     console.log( 'VIDEO STATUS => ' + httpVid.status );
-
+    
     var statusVideo1zu1 = checkVideo( '1zu1' );
     var statusText = checkText();
     //console.log( 'VIDEO STATUS => ', statusVideo1zu1, statusVideo1zu1 );
