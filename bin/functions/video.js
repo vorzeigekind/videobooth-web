5cec7faf4f72be305020f6a24077c012745e5f5b
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-08-31 13:07:30 +0200

    added log for video

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -4,6 +4,7 @@ import { json as config } from '../config.js';
 import { checkVideo } from './checks.js';
 
 export function loadVideo( id ){
+    console.log( 'LOADING VIDEO' );
     var video1zu1 = document.getElementById( 'player-video-1zu1' ).innerHTML;
     var button1zu1 = document.getElementById( 'button-video-1zu1' ).innerHTML;
     video1zu1 = video1zu1.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + config.video.token, );
