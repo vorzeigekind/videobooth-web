d98b3aa4408648d32c2d285d5936cf5aa2265534
Alexander Hesse <143098392+axeloni@users.noreply.github.com>
2023-09-04 19:03:02 +0200

    button normal

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -73,14 +73,14 @@ function loadVideo( id ){
     button1zu1.classList.add( 'video-accessable' );
     video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     //button1zu1.innerHTML = button1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
-    /* var statusVideo = checkVideo();
+    var statusVideo = checkVideo();
     if ( statusVideo == true ){
         console.log( 'WEB APP => making video button 2 visible ⬇️' );
 
         var video = document.getElementById( 'wrap-button-video' );
         //video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + '?' + config.video.token );
         video.classList.add( 'video-accessable' );
-    } */
+    }
 }
 
 // change video urls
