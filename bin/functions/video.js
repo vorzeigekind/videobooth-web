35e7cd3442666447a57d23390df47f4635969425
axeloni <hesse@vorzeigekind.de>
2023-09-14 15:56:38 +0200

    changed video to 16:9

--- bin/functions/video.js
+++ bin/functions/video.js
@@ -15,7 +15,6 @@ function addButtons( id ){
             const a = document.createElement("a");
             a.style.display = "none";
             a.href = url;
-            // the filename you want
             a.download = id + "-1zu1.mp4";
             document.body.appendChild(a);
             a.click();
@@ -23,7 +22,7 @@ function addButtons( id ){
             //alert("Your download in 1:1 is ready!"); // or you know, something with better UX...
             goToSection( 'video' );
         })
-        .catch(() => alert("oh no!"));
+        .catch(() => alert("Error downloading!"));
     };
     document.querySelector( '#wrap-button-video' ).onclick = function(){
         fetch(config.video.blob + id + '.' + config.video.filetype)
@@ -34,7 +33,6 @@ function addButtons( id ){
             const a = document.createElement("a");
             a.style.display = "none";
             a.href = url;
-            // the filename you want
             a.download = id + ".mp4";
             document.body.appendChild(a);
             a.click();
@@ -42,7 +40,7 @@ function addButtons( id ){
             //alert("Your download in 16:9 is ready!"); // or you know, something with better UX...
             goToSection( 'video' );
         })
-        .catch(() => alert("oh no!"));
+        .catch(() => alert("Error downloading!"));
     };
 }
 
@@ -51,19 +49,18 @@ function loadVideo( id ){
     console.log( 'WEB APP => making video player visible 🎥' );
     console.log( 'WEB APP => making video button 1 visible ⬇️' );
 
-    document.querySelector( '.elementor-video' ).src = config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token;
+    //document.querySelector( '.elementor-video' ).src = config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token;
+    document.querySelector( '.elementor-video' ).src = config.video.blob + id + '.' + config.video.filetype + '?' + config.video.token;
     var video1zu1 = document.getElementById( 'player-video-1zu1' );
     var button1zu1 = document.getElementById( 'wrap-button-video-1zu1' );
     video1zu1.classList.add( 'video-accessable' );
     button1zu1.classList.add( 'video-accessable' );
     video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
-    //button1zu1.innerHTML = button1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
     var statusVideo = checkVideo();
     if ( statusVideo == true ){
         console.log( 'WEB APP => making video button 2 visible ⬇️' );
 
         var video = document.getElementById( 'wrap-button-video' );
-        //video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + '?' + config.video.token );
         video.classList.add( 'video-accessable' );
     }
 }
@@ -72,16 +69,9 @@ function loadVideo( id ){
 function switchLink( id ){
     console.log( 'WEB APP => changing video urls 🔗' );
 
-    document.querySelector( '.elementor-video' ).src = config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token;
-    
-    
-    //var video1zu1 = document.getElementById( 'player-video-1zu1' );
-    //var button1zu1 = document.getElementById( 'wrap-button-video-1zu1' );
-    //var video = document.getElementById( 'wrap-button-video' );
-    //video1zu1.innerHTML = video1zu1.innerHTML.replace( 'https://videoboothfiles/', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
-    //button1zu1.innerHTML = button1zu1.innerHTML.replace( 'http://videolink1zu1', config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token );
-    //video.innerHTML = video.innerHTML.replace( 'http://videolink', config.video.blob + id + '.' + config.video.filetype + '?' + config.video.token );
-    
+    //document.querySelector( '.elementor-video' ).src = config.video.blob + '1zu1' + id + '.' + config.video.filetype + '?' + config.video.token;
+    document.querySelector( '.elementor-video' ).src = config.video.blob + id + '.' + config.video.filetype + '?' + config.video.token;
+
     addButtons( id );
 }
 
